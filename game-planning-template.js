a4fe27c5761af4d3236a8d56427b29ef3b02cbef
josyez <111254886+josyez@users.noreply.github.com>
2022-10-31 00:24:51 -0400

    Add files via upload

---
+++ game-planning-template.js
@@ -0,0 +1,42 @@
+// Game Title : Connect Four
+
+    /*General Overview of Gameplay 
+
+Game starts with a player vs computer. 
+Each player will be assigned a color for their discs. 
+The goal of the game is to connect four of the same color discs in a row to win. 
+Each player take turn inserting discs. Only one piece can be played at a time.
+The game ends when a player had connected all four discs or theres is a stalemate. 
+
+*/
+
+/* Requirements
+user interaction causes changes to the web page using DOM methods
+(at least 3 changes during game play) can be images, text, score indicator
+- Show image of connect four board. Show player and machine assigned color. (maybe i can let player chose their color??)
+- Display who's turn it currently is. 
+- Show score board. 
+- Display win or lose message
+
+Correct use of at least 1 JavaScript Class that is used in the game 
+- class will be player and machine. 
+
+Use of a method defined in your class 
+- placeDisc. will show where player or machine placed their disc.
+
+Keep score - user must be able to gain points
+- Player will gain points if they are able to connect 4 discs ina row.
+
+Declare if the user won or lost at the end of the game and display on the page 
+- Player or Machine will win, if the one is able to connect four first. 
+
+Use of CSS to set colors, fonts and overall layout of the page
+- Make a centered grid
+- make two different colors for discs
+- Align text
+
+Use of iteration. examples: for loop, while loop, .forEach method, .map method
+- squares, it will record where player had placed their discs
+- displayCurrentPlayer, to show who's turn it is.
+
+*/
