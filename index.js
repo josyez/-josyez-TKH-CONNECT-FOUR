a52c52df63b2100cf2a5f860834faae3703a0a7f
Joselyn <cuzco.joselyn@gmail.com>
2022-11-07 16:25:57 -0500

    added some things

--- index.js
+++ index.js
@@ -68,11 +68,62 @@ function setDisc(){
 
 }
 
-function checkWinner(){
-    for (let r =0; r < row; r++){
-        for (let index = 0; index < array.length; index++) {
-            const element = array[index];
-            
+function checkWinner() {
+    // horizontal
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns - 3; c++){
+           if (board[r][c] != ' ') {
+               if (board[r][c] == board[r][c+1] && board[r][c+1] == board[r][c+2] && board[r][c+2] == board[r][c+3]) {
+                   setWinner(r, c);
+                   return;
+               }
+           }
         }
-    }
+   }
+
+   // vertical
+   for (let c = 0; c < columns; c++) {
+       for (let r = 0; r < rows - 3; r++) {
+           if (board[r][c] != ' ') {
+               if (board[r][c] == board[r+1][c] && board[r+1][c] == board[r+2][c] && board[r+2][c] == board[r+3][c]) {
+                   setWinner(r, c);
+                   return;
+               }
+           }
+       }
+   }
+
+   // anti diagonal
+   for (let r = 0; r < rows - 3; r++) {
+       for (let c = 0; c < columns - 3; c++) {
+           if (board[r][c] != ' ') {
+               if (board[r][c] == board[r+1][c+1] && board[r+1][c+1] == board[r+2][c+2] && board[r+2][c+2] == board[r+3][c+3]) {
+                   setWinner(r, c);
+                   return;
+               }
+           }
+       }
+   }
+
+   // diagonal
+   for (let r = 3; r < rows; r++) {
+       for (let c = 0; c < columns - 3; c++) {
+           if (board[r][c] != ' ') {
+               if (board[r][c] == board[r-1][c+1] && board[r-1][c+1] == board[r-2][c+2] && board[r-2][c+2] == board[r-3][c+3]) {
+                   setWinner(r, c);
+                   return;
+               }
+           }
+       }
+   }
+}
+
+function setWinner(r, c) {
+   let winner = document.getElementById("winner");
+   if (board[r][c] == playerRed) {
+       winner.innerText = "Red Player Wins";             
+   } else {
+       winner.innerText = "Yellow Player Wins";
+   }
+   gameOver = true;
 }
